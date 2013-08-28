622bc9da3ac4c07cef02382c5590e95548c87dcc
Anton Berezin <tobez@tobez.org>
2013-08-28 13:36:20 +0200

    Release 0.60.

--- lib/DBIx/Perlish.pm
+++ lib/DBIx/Perlish.pm
@@ -10,7 +10,7 @@ use vars qw($VERSION @EXPORT @EXPORT_OK %EXPORT_TAGS $SQL @BIND_VALUES);
 require Exporter;
 use base 'Exporter';
 
-$VERSION = '0.59';
+$VERSION = '0.60';
 @EXPORT = qw(db_fetch db_select db_update db_delete db_insert sql);
 @EXPORT_OK = qw(union intersect except);
 %EXPORT_TAGS = (all => [@EXPORT, @EXPORT_OK]);
@@ -423,7 +423,7 @@ DBIx::Perlish - a perlish interface to SQL databases
 
 =head1 VERSION
 
-This document describes DBIx::Perlish version 0.59
+This document describes DBIx::Perlish version 0.60
 
 
 =head1 SYNOPSIS
