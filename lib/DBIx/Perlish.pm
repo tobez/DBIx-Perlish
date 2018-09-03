9b3063066b1db40d1a96a78be1548993d8fae87d
Dmitry Karasik <dmitry.karasik@teliacompany.com>
2018-09-03 15:19:21 +0200

    add bin/perlish-parse

--- lib/DBIx/Perlish.pm
+++ lib/DBIx/Perlish.pm
@@ -10,7 +10,7 @@ require Exporter;
 use base 'Exporter';
 use Keyword::Pluggable;
 
-$VERSION = '1.00';
+$VERSION = '1.01';
 @EXPORT = qw(sql);
 @EXPORT_OK = qw(union intersect except subselect);
 %EXPORT_TAGS = (all => [@EXPORT, @EXPORT_OK]);
