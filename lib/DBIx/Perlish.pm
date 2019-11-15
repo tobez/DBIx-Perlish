2263ed0e6f55cc1acb71a17a2ba2bae16ced90a4
Dmitry Karasik <dmitry.karasik@teliacompany.com>
2019-11-15 12:58:44 +0100

    v1.04

--- lib/DBIx/Perlish.pm
+++ lib/DBIx/Perlish.pm
@@ -10,7 +10,7 @@ require Exporter;
 use base 'Exporter';
 use Keyword::Pluggable;
 
-$VERSION = '1.03';
+$VERSION = '1.04';
 @EXPORT = qw(sql);
 @EXPORT_OK = qw(union intersect except subselect);
 %EXPORT_TAGS = (all => [@EXPORT, @EXPORT_OK]);
